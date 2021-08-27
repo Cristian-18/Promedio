7a97f61048a17f2fa83fb993aad2d3cfb1f0d4da
Cristian Canto <cristiancantobueno1818@gmail.com>
2021-08-27 01:23:33 -0500

    Se le dio interactividad al boton de PROMEDIO

--- promedio.js
+++ promedio.js
@@ -1,20 +1,48 @@
 
+function perimetroCuadrado (lado){
+    return lado * 4;
+}
 
-function calcularPromedio (lista){
-   /*  let sumaLista=0;
+function areaCuadrado (lado){
+    return lado*lado;
+}
+
+function calcularPerimetro(){
+    const entrada = document.getElementById("lado-cuadrado");
+    const value = entrada.value;
+
+    const resultado = perimetroCuadrado(value);
+    alert(resultado);
+}
+
+function calcularArea (){
+    const ingLado= document.getElementById("lado-cuadrado");
+    const valor =ingLado.value;
+
+    result = areaCuadrado(valor);
+    alert (result);
+}
+
+/* function calcularPromedio (lista){
+    let sumaLista=0;
     for (i=0;i<lista.length;i++){
         sumaLista = sumaLista + lista[i];
-    } */
+    }
 
+  ===========================================
     const sumarLista = lista.reduce(
         function (valorAcumulado=0,nuevoValor){
             return valorAcumulado + nuevoValor;
+            
         }
-    )
+        =============================================
+     
+        
 
+    console.log("El lado de mi cuadrado mide:" + lista);
     const promedio = sumarLista/lista.length;
 
     return promedio;
 
-}
+} */
 
